323df72f83d4e962459f53fc5910f4e41ccedc98
Ankit7678 <98596964+Ankit7678@users.noreply.github.com>
2022-06-26 22:44:35 +0530

    Add files via upload

---
+++ PersonalInfo.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function PersonalInfo({ formData, setFormData }) {
+  return (
+    <div className="personal-info-container">
+      <input
+        type="text"
+        placeholder="First Name..."
+        value={formData.firstName}
+        onChange={(e) => {
+          setFormData({ ...formData, firstName: e.target.value });
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Last Name..."
+        value={formData.lastName}
+        onChange={(e) => {
+          setFormData({ ...formData, lastName: e.target.value });
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Username..."
+        value={formData.username}
+        onChange={(e) => {
+          setFormData({ ...formData, username: e.target.value });
+        }}
+      />
+        <input
+        type="text"
+        placeholder="Phone no..."
+        value={formData.phoneno}
+        onChange={(e) => {
+          setFormData({ ...formData, username: e.target.value });
+        }}
+      />
+    </div>
+  );
+}
+
+export default PersonalInfo;
